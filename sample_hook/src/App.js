cb482002ff347a44114e3bf7a22672ee7765b656
yanamadalarajesh <rajesh2012y@gmail.com>
2024-03-10 22:41:56 +0530

    starting changes

--- sample_hook/src/App.js
+++ sample_hook/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [employeeName, setEmployeeName] = useState([
+    { id: 1, name: "karthik" },
+    { id: 2, name: "rambabu" },
+    { id: 3, name: "rajesh" },
+    { id: 4, name: "goutham" },
+    { id: 5, name: "manvith" },
+    { id: 6, name: "venkat" },
+    { id: 7, name: "kruthvika" },
+  ]);
+  const [enable, setEnable] = useState(null);
+
+  const handleChangeEmployeeName = (id) => {
+    // Enable editing for the clicked employee
+    setEnable(id);
+  };
+
+  const handleInputChange = (id, newName) => {
+    // Update the name of the employee
+    const updatedEmployees = employeeName.map((employee) =>
+      employee.id === id ? { ...employee, name: newName } : employee
+    );
+    setEmployeeName(updatedEmployees);
+    // Disable editing after name change
+    // setEnable(null);
+  };
+
+  const handleEnableInput = () => {
+    setEnable(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Names</h1>
+      <ul className="buttonList">
+        {employeeName.map((employee) => (
+          <li key={employee.id} className="list">
+            <input
+              value={employee.name}
+              type="text"
+              disabled={enable !== employee.id}
+              onChange={(e) => handleInputChange(employee.id, e.target.value)}
+              onBlur={handleEnableInput}
+            />
+            <button
+              onClick={() => handleChangeEmployeeName(employee.id)}
+              className="button"
+            >
+              Edit
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
